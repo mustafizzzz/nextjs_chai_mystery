beeb703d66aac40f53c7b0b9c247d438561ac60a
Mustafiz Ansari <mustafizansari35@gmail.com>
2024-06-23 02:34:04 +0530

    chore: Refactor dashboard page component and add user check

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -16,7 +16,7 @@ import { useSession } from "next-auth/react";
 import { useCallback, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
-const page = () => {
+const UserDashboard = () => {
     const [messages, setMessages] = useState<Message[]>([]);
     const [isLoading, setIsLoading] = useState(false);
     const [isSwitchLoading, setIsSwitchLoading] = useState(false);
@@ -126,6 +126,14 @@ const page = () => {
             description: 'Profile URL copied to clipboard'
         })
     }
+    if (!session || !session.user) {
+        return (
+            <>
+                <h1>No Data found</h1>
+            </>
+        )
+
+    }
 
     return (
         <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
@@ -188,4 +196,4 @@ const page = () => {
     )
 }
 
-export default page
+export default UserDashboard
