c377de3d4201fa3fc9c92a72ee16b6a09f41b055
Mustafiz Ansari <mustafizansari35@gmail.com>
2024-06-22 20:58:04 +0530

    feat: Add sign-in page with form validation and error handling

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,16 +1,135 @@
-'use client'
-import { useSession, signIn, signOut } from "next-auth/react"
-export default function Component() {
-  const { data: session } = useSession()
+// import { useSession, signIn, signOut } from "next-auth/react"
+// export default function Component() {
+//   const { data: session } = useSession()
 
-  if (session) {
-    return (
-      <>
-        <p>Signed in as {session.user.name}</p>
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+//   if (session) {
+//     return (
+//       <>
+//         <p>Signed in as {session.user.name}</p>
+//         <button onClick={() => signOut()}>Sign out</button>
+//       </>
+//     )
+//   }
 
-  return <a href="/api/auth/signin">Sign in</a>
+//   return <a href="/api/auth/signin">Sign in</a>
+// }
+
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import Link from 'next/link';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useToast } from '@/components/ui/use-toast';
+import { Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { signInSchema } from '@/schemas/signInSchema';
+import { signIn } from 'next-auth/react';
+
+export default function SignUpForm() {
+
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    const result = await signIn('credentials', {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password,
+    })
+
+    if (result?.error) {
+
+      if (result?.error === 'CredentialsSignin') {
+        toast({
+          title: 'Login failed',
+          description: 'Incorrect email or password',
+          variant: 'destructive'
+        })
+
+      }
+
+      toast({
+        title: 'Login failed',
+        description: 'Incorrect email or password',
+        variant: 'destructive'
+      })
+    }
+
+    if (result?.url) {
+      router.replace('/dashboard');
+
+    }
+
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-200">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join Mystery Mail
+          </h1>
+          <p className="mb-4">Sign In to start your anonymous adventure</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/username</FormLabel>
+                  <Input {...field} name="email/username" />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" {...field} name="password" />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className='w-full'>
+              Signin
+            </Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Create a account?{' '}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -8,81 +8,81 @@ import UserModel from '@/models/User';
 
 export const authOptions: NextAuthOptions = {
 
-    providers: [
-        CredentialsProvider({
-            id: 'credentials',
-            name: 'Credentials',
-            credentials: {
-                email: { label: "Email", type: "text" },
-                password: { label: "Password", type: "password" }
-            },
-            async authorize(credentials: any, req): Promise<any> {
-                await dbConnect();
-                try {
-                    const user = await UserModel.findOne({
-                        $or: [
-                            { email: credentials.identifier },
-                            { username: credentials.identifier }
-                        ]
-                    })
+  providers: [
+    CredentialsProvider({
+      id: 'credentials',
+      name: 'Credentials',
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials: any, req): Promise<any> {
+        await dbConnect();
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifier },
+              { username: credentials.identifier }
+            ]
+          })
 
-                    if (!user) {
-                        throw new Error('No user found with this email');
-                    }
+          if (!user) {
+            throw new Error('No user found with this email');
+          }
 
-                    if (!user.isVerified) {
-                        throw new Error('User not verified');
-                    }
+          if (!user.isVerified) {
+            throw new Error('User not verified');
+          }
 
-                    const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password);
-                    if (isPasswordCorrect) {
-                        return user;
-                    } else {
-                        throw new Error('Invalid password');
-                    }
+          const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password);
+          if (isPasswordCorrect) {
+            return user;
+          } else {
+            throw new Error('Invalid password');
+          }
 
-                } catch (error) {
-                    throw new Error('Error in credentials');
-                }
-
-            }
-
-
-        })
-    ],
-
-    pages: {
-        signIn: '/sign-in',
-    },
-
-    session: {
-        strategy: 'jwt',
-    },
-
-    secret: process.env.NEXTAUTH_SECRET,
-
-    callbacks: {
-        async session({ session, token }) {
-            if (token) {
-                session.user._id = token._id;
-                session.user.isVerified = token.isVerified;
-                session.user.isAcceptingMessages = token.isAcceptingMessages;
-                session.user.username = token.username;
-
-            }
-            return session
-        },
-        async jwt({ token, user }) {
-            if (user) {
-                token._id = user._id?.toString();
-                token.isVerified = user.isVerified;
-                token.isAcceptingMessages = user.isAcceptingMessages;
-                token.username = user.username;
-            }
-            return token
+        } catch (error) {
+          throw new Error('Error in credentials');
         }
 
+      }
+
+
+    })
+  ],
+
+  pages: {
+    signIn: '/sign-in',
+  },
+
+  session: {
+    strategy: 'jwt',
+  },
+
+  secret: process.env.NEXTAUTH_SECRET,
+
+  callbacks: {
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id;
+        session.user.isVerified = token.isVerified;
+        session.user.isAcceptingMessages = token.isAcceptingMessages;
+        session.user.username = token.username;
+
+      }
+      return session
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString();
+        token.isVerified = user.isVerified;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+        token.username = user.username;
+      }
+      return token
     }
+
+  }
 
 }
 
