ae68f9b0b0bf16f7f470290959fcd1ece64cf277
Mustafiz Ansari <mustafizansari35@gmail.com>
2024-07-02 20:16:29 +0530

    chore: Update package dependencies and add Google Generative AI library

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,10 +1,16 @@
 import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 import { NextResponse } from 'next/server';
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-});
+// const openai = new OpenAI({
+//     apiKey: "sk-proj-Pq8eF2oQ6PZXsegcT7ZLT3BlbkFJlyw65qNkANj8tBUj231p",
+//     //process.env.OPENAI_API_KEY,
+// });
+
+const genAI = new GoogleGenerativeAI("AIzaSyAQXXRZjtLLf55rG6w1Wc0znHOavPj20xc");
+
+
 
 export const runtime = 'edge';
 
@@ -13,25 +19,58 @@ export async function POST(req: Request) {
         const prompt =
             "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
-        const response = await openai.completions.create({
-            model: 'gpt-3.5-turbo-instruct',
-            max_tokens: 400,
-            stream: true,
-            prompt,
-        });
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+        const text = response.text();
+        console.log(text);
 
-        const stream = OpenAIStream(response);
-        return new StreamingTextResponse(stream);
+        if (!response) {
+            return Response.json({
+                success: false,
+                message: 'Error Error suggest messages. Please try again later.'
+            }, {
+                status: 500
+            }
+            )
+        } else {
+            return Response.json({
+                success: true,
+                data: text
+            })
+
+        }
+
+
+        // const response = await openai.completions.create({
+        //     model: 'gpt-3.5-turbo',
+        //     max_tokens: 400,
+        //     stream: true,
+        //     prompt,
+        // });
+
+        // const stream = OpenAIStream(response);
+        // return new StreamingTextResponse(stream);
 
     } catch (error) {
-        if (error instanceof OpenAI.APIError) {
-            // OpenAI API error handling
-            const { name, status, headers, message } = error;
-            return NextResponse.json({ name, status, headers, message }, { status });
-        } else {
-            // General error handling
-            console.error('An unexpected error occurred:', error);
-            throw error;
+        // if (error instanceof OpenAI.APIError) {
+        //     // OpenAI API error handling
+        //     const { name, status, headers, message } = error;
+        //     return NextResponse.json({ name, status, headers, message }, { status });
+        // } else {
+        //     // General error handling
+        //     console.error('An unexpected error occurred:', error);
+        //     throw error;
+        // }
+
+
+        console.error('Error suggest messages: ', error);
+        return Response.json({
+            success: false,
+            message: 'Error Error suggest messages. Please try again later.'
+        }, {
+            status: 500
         }
+        )
     }
 }
--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -13,7 +13,7 @@ const page = () => {
                     <textarea
                         className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                         placeholder="Write your anonymous message here"
-                        rows={4}
+                        rows={2}
                     ></textarea>
                     <button className="w-full mt-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-400">
                         Send It
@@ -40,7 +40,7 @@ const page = () => {
                 </div>
 
                 {/* Footer Heading */}
-                <div className="mt-8 text-center">
+                <div className="mt-6 text-center">
                     <h2 className="text-xl font-semibold">Get started with your anonymous feedback</h2>
                 </div>
             </div>
